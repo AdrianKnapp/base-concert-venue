8f32e5ccc43cbd4bbe2ec26b06869265cba4f197
AdrianKnapp <adrknapp@gmail.com>
2022-09-01 18:45:37 -0300

    Added handler on msw

--- base-concert-venue/__tests__/__mocks__/msw/handlers.js
+++ base-concert-venue/__tests__/__mocks__/msw/handlers.js
@@ -1,3 +1,17 @@
 import { rest } from "msw";
 
-export const handlers = [];
+import { readFakeData } from "@/__tests__/__mocks__/fakeData";
+
+export const handlers = [
+  rest.get(
+    "http://localhost:3000/api/shows/:showId",
+    async (request, response, context) => {
+      const { fakeShows } = await readFakeData();
+      return response(
+        context.json({
+          show: fakeShows[0],
+        })
+      );
+    }
+  ),
+];
