f0ff637ff9450ea62ed18dd062aa5f770d03ce30
AdrianKnapp <adrknapp@gmail.com>
2023-01-10 21:28:18 -0300

    Added cypress test to run after build

--- base-concert-venue/cypress/e2e/routes.cy.ts
+++ base-concert-venue/cypress/e2e/routes.cy.ts
@@ -1,3 +1,6 @@
+import { generateNewBand } from "@/__tests__/__mocks__/fakeData/newBand";
+import { generateRandomId } from "@/lib/features/reservations/utils";
+
 describe("Routes", () => {
   it("displays correct heading when navigation to shows route", () => {
     cy.visit("/");
@@ -17,6 +20,29 @@ describe("Routes", () => {
 
   it("resets the db", () => {
     cy.task("db:reset");
+  });
+
+  it("displays correct band name for band route that existed at build time", () => {
+    cy.task("db:reset").visit("/bands/1");
+    cy.get("h2")
+      .first()
+      .contains(/shamrock pete/i);
+  });
+
+  it("displays correct error message when band is not found", () => {
+    cy.task("db:reset").visit("/bands/12345");
+    cy.get("h2")
+      .first()
+      .contains(/error: band not found/i);
+  });
+
+  it("displays name for band that was not present at build time", () => {
+    const bandId = generateRandomId();
+    const newBand = generateNewBand(bandId);
+    cy.task("db:reset").task("addBand", newBand).visit(`/bands/${bandId}`);
+    cy.get("h2")
+      .first()
+      .contains(/avalanche of cheese/i);
   });
 });
 
