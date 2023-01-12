7c4527cdd29c2dbb73a86cee86235e67073a7011
AdrianKnapp <adrknapp@gmail.com>
2023-01-12 10:37:15 -0300

    Added isr revalidate test for new show

--- base-concert-venue/cypress/e2e/isr-revalidate.cy.ts
+++ base-concert-venue/cypress/e2e/isr-revalidate.cy.ts
@@ -1,4 +1,5 @@
 import { generateNewBand } from "@/__tests__/__mocks__/fakeData/newBand";
+import { generateNewShow } from "@/__tests__/__mocks__/fakeData/newShow";
 import { generateRandomId } from "@/lib/features/reservations/utils";
 
 describe("ISR Revalidation", () => {
@@ -27,6 +28,33 @@ describe("ISR Revalidation", () => {
       .should("exist");
 
     // Clear ISR cache to initial db conditions
+    cy.resetDbAndIsrCache();
+  });
+
+  it("should load refreshed page from cache after new show is added", () => {
+    // check if show is not on page
+    cy.task("db:reset").visit("/shows");
+    cy.get("h2")
+      .contains(/avalanche of cheese/i)
+      .should("not.exist");
+
+    // create show
+    const showId = generateRandomId();
+    const newShow = generateNewShow(showId);
+    const secret = Cypress.env("REVALIDATION_SECRET");
+    cy.request("POST", `api/shows?secret=${secret}`, {
+      newShow,
+    }).then((res) => {
+      expect(res.body.revalidated).to.equal(true);
+    });
+
+    // check if show exist
+    cy.reload();
+    cy.get("h2")
+      .contains(/avalanche of cheese/i)
+      .should("exist");
+
+    // Reset ISR cache
     cy.resetDbAndIsrCache();
   });
 });
