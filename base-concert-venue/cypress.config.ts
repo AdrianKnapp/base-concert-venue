28ef981bda392ed801ee4a339ac92443bd1cafad
AdrianKnapp <adrknapp@gmail.com>
2023-01-13 09:37:28 -0300

    Added swr test

--- base-concert-venue/cypress.config.ts
+++ base-concert-venue/cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "cypress";
 
 import { resetDB } from "./__tests__/__mocks__/db/utils/reset-db";
 import { addBand } from "./lib/features/bands/queries";
+import { addReservation } from "./lib/features/reservations/queries";
 
 export default defineConfig({
   e2e: {
@@ -12,6 +13,8 @@ export default defineConfig({
       on("task", {
         "db:reset": () => resetDB().then(() => null),
         addBand: (newBand) => addBand(newBand).then(() => null),
+        addReservation: (newReservation) =>
+          addReservation(newReservation).then(() => null),
       });
 
       return config;
---
+++ base-concert-venue/cypress/e2e/swr-revalidation.cy.ts
@@ -0,0 +1,41 @@
+import { generateNewReservation } from "@/__tests__/__mocks__/fakeData/newReservation";
+import { generateRandomId } from "@/lib/features/reservations/utils";
+
+const ONE_SECOND = 1000;
+const THIRTY_SECONDS = 30 * ONE_SECOND;
+
+describe("swr revalidation", () => {
+  it("should refresh the shows page after 30 seconds", () => {
+    cy.clock();
+    cy.task("db:reset").visit("/shows");
+
+    cy.findAllByText(/sold out/i).should("have.length", 1);
+
+    const newReservation = generateNewReservation({
+      reservationId: generateRandomId(),
+      showId: 0,
+      seatCount: 10,
+    });
+
+    cy.task("addReservation", newReservation);
+
+    cy.tick(ONE_SECOND);
+    cy.findAllByText(/sold out/i).should("have.length", 1);
+
+    cy.tick(THIRTY_SECONDS);
+    cy.findAllByText(/sold out/i).should("have.length", 2);
+  });
+
+  // # REVIEW: https://www.udemy.com/course/nextjs-testing/learn/lecture/32204062#questions/19007238
+  // it("should show correct ticket value after buy one", () => {
+  //   cy.clock();
+  //   cy.task("db:reset").visit("/reservations/0");
+
+  //   // login
+  //   cy.findByRole("main").within(() => {
+  //     cy.findByRole("button", { name: /sign in/i }).click();
+  //   });
+  // });
+});
+
+export default {};
